3ee5f979b31e2bbe585f922686ab755a58d97e09
Bosco Ho <apposite@gmail.com>
2022-11-20 20:06:29 +1100

    hollow 1.3

--- pdbstruct/__init__.py
+++ pdbstruct/__init__.py
@@ -1,5 +1,7 @@
-from molecule import *
-from polymer import *
-from protein import *
-from soup import *
-from vector3d import *
+from __future__ import absolute_import
+
+from .molecule import *
+from .polymer import *
+from .protein import *
+from .soup import *
+from .vector3d import *
